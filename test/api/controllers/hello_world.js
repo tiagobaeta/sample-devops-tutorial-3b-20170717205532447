b7210adc9928b560d72a685a63be80653228d8f9
tiagobaeta <tiagobaeta@gmail.com>
2017-07-18 19:45:50 +0000

    Change strange to stranger

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello there, strange!');
+            res.body.should.eql('Hello there, stranger!');
 
             done();
           });
